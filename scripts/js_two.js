d7714f77875b39d89b22c78eeb91fca844fb6b49
jengleberg <jeffengleberg@hotmail.com>
2017-09-29 08:50:04 -0600

    finalizes js notes

--- scripts/js_two.js
+++ scripts/js_two.js
@@ -1,7 +1,5 @@
 
 // Selector Elements from HTML
-
-
 var p1Name = document.getElementById("p1name");
 var p2Name = document.getElementById("p2name");
 var p1RoundScore = document.getElementById("p1score");
@@ -14,7 +12,6 @@ var guessButton = document.getElementById("guessButton");
 
 
 // Game Variables
-
 var puzzles = ["javascript", "function", "general-assembly", "prototype", "binary", "psuedocode"];
 var puzzle = "";
 var answers = [];
@@ -25,33 +22,21 @@ var pScore = {p1RoundScore: 0, p2RoundScore: 0};
 var round = 1;
 var prize = "";
 
-
-
 // Spin Results
-
 spinValues = ['LoseTurn', '800', '350', '450', '700', '300', '600', '5000', '300', '600',
 				'300', '500', '800', '550', '400', '300', '900', '500', '300', '900', 'Bankrupt', '600', '400', '300'];
 
 // Function that pulls a random value from the spinValues array
-
- function spinAmount() {
+function spinAmount() {
 	 return spinValues[Math.floor((Math.random()*spinValues.length))];
 }
 
 // Event Listener when spin button is clicked to return the spin amount.
-
 spin.addEventListener('click', function() {
 	alert(spinAmount());
 });
 	
-	
-
-
-
-
-
 // Function that initializes the game by randomly choosing a puzzle from the puzzles array
-
 function init() {
 	puzzle = puzzles[Math.floor(Math.random() * puzzles.length)]; 
 
@@ -60,86 +45,71 @@ function init() {
 	for (var i = 0; i < puzzle.length; i++) {
 	answers[i] = "_";
 }
-
+// Joins the answer with the random puzzle and displays the message let's play
 	document.getElementById("answer").innerHTML= answers.join(" ");
     document.getElementById("message").innerHTML= "LET'S PLAY!";
 }
 
-
-
-
+// initializes the first puzzle
 init();
 
-
-
 // function that allows player to guess the letter
-
+// On clicking the guess button adds the input of the guess and compares it to the puzzle answer
 guessButton.addEventListener("click", function() {
 	var guess = document.getElementById("guess").value;
 	var showThisMessage = "";
+// If the guess button is clicked and nothing is in the input field display message	
 	if (guess.length === 0) {
 		showThisMessage = "Please enter a letter";
 	}
+// If the guess button is clicked and there are two letters in the input field display message	
 	else if (guess.length !== 1) {
 		showThisMessage = "Please enter only a single letter";
-	
-
+// Need to add logic for a non letter character and also to accept capital or lower case
 	} else {
-		
-		for (var i = 0; i < puzzle.length; i++) {
-			if (puzzle[i] === guess) {
-				answers[i] = guess;
-				showThisMessage= "YES! Vanna, show us " + guess + ".";
-				pScore.innerHTML=prize;
-				
-			} 
-				
-		}
-
-		var remainingLetters = 0;
+// If the guess is correct add it to the answer field(s) and display message
+	for (var i = 0; i < puzzle.length; i++) {
+		if (puzzle[i] === guess) {
+			answers[i] = guess;
+			showThisMessage= "YES! Vanna, show us " + guess + ".";
+			pScore.innerHTML=prize;
+		} 
+	}
+// To count remaining letters.  Should have counted correct letters for multiplying prize if correct guess
+	var remainingLetters = 0;
 		for (i = 0; i < puzzle.length; i++) {
 			if (answers[i] === '_') {
 				remainingLetters += 1;
 			}
 		}
+// if reamining letters in answer is zero the puzzle is solved		
 		if (remainingLetters == 0) {
 			showThisMessage = "YES! Solved the Puzzle!";
 		} 
-
+// If incorrect guess display message and run next player function
 		if (showThisMessage === "") {
 			showThisMessage = "Sorry, no " + guess + " " + currentPlayer + " Your turn";
 			nextPlayer();
-			
-
-			
-
 		} 
-
+// Code needs to be cleaned up
 		document.getElementById("answer").innerHTML = answers.join(" ");
-
 		document.getElementById("guess").innerHTML = showThisMessage;
 		}
 		document.getElementById("message").innerHTML = showThisMessage;
-	
+	});
 
-
-
-});
-
-
+// Next player function
 function nextPlayer() {
   if (currentPlayer == player1) {
   	currentPlayer = player2;
   } else {
   	currentPlayer = player1;
-
-  }
-
+	}
 }
 
+// First attempt at bankrupt and scoring function
 function updateScore (prize, numGuess, Bankrupt) {
 	if(Bankrupt) {
-
 	}
 }
 
